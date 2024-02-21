4c4f36b0a99da14007f03fc6630adaf25af8101d
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-02-21 22:07:17 +0500

    Update AnimeFusion.lua

--- AnimeFusion.lua
+++ AnimeFusion.lua
@@ -2,7 +2,7 @@ while not game:IsLoaded() do
     wait()
 end
 local libary = loadstring(game:HttpGet("https://pastebin.com/raw/RvJ0qewm", true))()
-local main = libary:CreateWindow("STS")
+local main = libary:CreateWindow("TRASH")
 
 local mod = main:Button("Click", function()
 _G.Click_enabled = not _G.Click_enabled
@@ -10,6 +10,20 @@ _G.Click_enabled = not _G.Click_enabled
 while _G.Click_enabled do
 game:GetService("ReplicatedStorage").Events.Click:FireServer()
 
+wait(0.1)
+end
+end)
+
+local mod = main:Button("Attack Egg", function()
+_G.AttackEgg_enabled = not _G.AttackEgg_enabled
+
+while _G.AttackEgg_enabled do
+local args = {
+    [1] = "Attack Egg",
+    [2] = "Single"
+}
+
+game:GetService("ReplicatedStorage").Functions.Hatch:InvokeServer(unpack(args))
 wait(0.1)
 end
 end)
